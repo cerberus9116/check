2698e23e28ade0ac5adce6af677e1efdbcb4b377
nandoslayer <92729509+nandoslayer@users.noreply.github.com>
2022-05-20 04:50:58 -0300

    Add files via upload

--- gestorssh/instpainel.sh
+++ gestorssh/instpainel.sh
@@ -11,6 +11,7 @@ echo ""
 echo -ne "\n\033[1;32mDE UM ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read -r
 clear
 echo -e "\n\033[1;36mINICIANDO INSTALAÇÃO \033[1;33mAGUARDE..."
+apt install unzip -y > /dev/null 2>&1
 apt-get install figlet -y > /dev/null 2>&1
 rm /bin/pweb > /dev/null 2>&1
 sleep 5
@@ -34,6 +35,16 @@ wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/updatepainelarm
 wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/versao > /dev/null 2>&1
 chmod 777 *.sh > /dev/null 2>&1
 clear
+rm *.sh* > /dev/null 2>&1
+rm *.zip > /dev/null 2>&1
+wget github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/painel.zip.001 > /dev/null 2>&1
+wget github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/painel.zip.002 > /dev/null 2>&1
+wget github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/painel.zip.003 > /dev/null 2>&1
+cat painel.zip* > painelweb.zip && zip -F painelweb.zip > /dev/null 2>&1
+rm -rf /opt/painel > /dev/null 2>&1
+unzip painelweb.zip -d  /opt/painel/ > /dev/null 2>&1
+chmod -R 777 /opt/painel > /dev/null 2>&1
+sleep 1
 echo -e "        \033[1;33m • \033[1;32mINSTALAÇÃO CONCLUÍDA\033[1;33m • \033[0m"
 echo ""
 echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mpweb\033[0m"
