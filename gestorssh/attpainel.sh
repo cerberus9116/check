edcff0707d68cc77a42fd3f2aeb813bebdd085e7
nandoslayer <92729509+nandoslayer@users.noreply.github.com>
2022-05-24 19:42:30 -0300

    Add files via upload

--- gestorssh/attpainel.sh
+++ gestorssh/attpainel.sh
@@ -25,15 +25,6 @@ wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/updatepainelarm
 wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/updatepainelarm18.sh > /dev/null 2>&1
 wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/versao > /dev/null 2>&1
 chmod 777 *.sh > /dev/null 2>&1
-echo ""
-echo ""
-echo -e "        \033[1;33m • \033[1;32mINSTALAÇÃO CONCLUÍDA\033[1;33m • \033[0m"
-echo ""
-echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mpweb\033[0m"
-echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@nandoslayer\033[0m"
-echo ""
-echo -ne "\n\033[1;31mENTER \033[1;33mpara retornar ao \033[1;32mPWEB! \033[0m"; read
 cat /dev/null > ~/.bash_history && history -c
-pweb
 }
 update_start
