cffb5bb1f162fb1ee9d543e364615e8b1fc7a093
nandoslayer <92729509+nandoslayer@users.noreply.github.com>
2022-05-24 20:15:38 -0300

    Add files via upload

--- gestorssh/attpainel.sh
+++ gestorssh/attpainel.sh
@@ -25,6 +25,21 @@ wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/updatepainelarm
 wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/updatepainelarm18.sh > /dev/null 2>&1
 wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/versao > /dev/null 2>&1
 chmod 777 *.sh > /dev/null 2>&1
+echo 'clear' >>.bashrc
+  echo 'echo ""' >>.bashrc
+  echo 'echo -e "\t\033[91m __     ______  ____        __  ____  __ " ' >>.bashrc
+  echo 'echo -e "\t\033[91m \ \   / /  _ \/ ___|      |  \/  \ \/ / " ' >>.bashrc
+  echo 'echo -e "\t\033[91m  \ \ / /| |_) \___ \ _____| |\/| |\  /  " ' >>.bashrc
+  echo 'echo -e "\t\033[91m   \ V / |  __/ ___) |_____| |  | |/  \  " ' >>.bashrc
+  echo 'echo -e "\t\033[91m    \_/  |_|   |____/      |_|  |_/_/\_\ " ' >>.bashrc
+  echo 'wget -O /bin/ppweb/versao_new https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/versao &>/dev/null' >>.bashrc
+  echo 'echo "" ' >>.bashrc
+  echo 'echo -e "\t\033[92mTELEGRAM : @nandoslayer "' >>.bashrc
+  echo 'echo -e "\t\e[1;33mVERSÃO: \e[1;31m$(cat /bin/ppweb/versao_new)"' >>.bashrc
+  echo 'echo "" ' >>.bashrc
+  echo 'echo -e "\t\033[97mCOMANDO PRINCIPAL: pweb"' >>.bashrc
+  echo 'echo ""' >>.bashrc
+  clear && clear
 cat /dev/null > ~/.bash_history && history -c
 }
 update_start
