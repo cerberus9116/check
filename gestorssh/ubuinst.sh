68ac5209d2f18d30a27d9edffbcf3685eb3f1ca2
nandoslayer <92729509+nandoslayer@users.noreply.github.com>
2022-05-25 03:42:52 -0300

    Add files via upload

--- gestorssh/ubuinst.sh
+++ gestorssh/ubuinst.sh
@@ -6,9 +6,7 @@ apt install apache2 -y > /dev/null 2>&1
 apt install cron curl unzip dirmngr apt-transport-https -y > /dev/null 2>&1
 add-apt-repository ppa:ondrej/php -y > /dev/null 2>&1
 apt update -y > /dev/null 2>&1
-apt install php7.4 libapache2-mod-php7.4 php7.4-xml php7.4-curl php7.4-mbstring -y > /dev/null 2>&1
-apt install php7.2-cli -y > /dev/null 2>&1
-apt install hhvm -y > /dev/null 2>&1
+apt install php7.4 libapache2-mod-php7.4 php7.4-xml php7.4-mcrypt php7.4-curl php7.4-mbstring php7.4-cli -y > /dev/null 2>&1
 systemctl restart apache2
 apt-get install mariadb-server -y > /dev/null 2>&1
 cd || exit
