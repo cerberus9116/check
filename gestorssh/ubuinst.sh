36924f0e2e4eec87e7337a07acdf7a374d09fb05
nandoslayer <92729509+nandoslayer@users.noreply.github.com>
2022-05-24 23:02:46 -0300

    Add files via upload

--- gestorssh/ubuinst.sh
+++ gestorssh/ubuinst.sh
@@ -1,101 +1,12 @@
 #!/bin/bash
-msg() {
-   BRAN='\033[1;37m' && RED='\e[31m' && GREEN='\e[32m' && YELLOW='\e[33m'
-  BLUE='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' && BLACK='\e[1m' && SEMCOR='\e[0m'
-  case $1 in
-  -ne) cor="${RED}${BLACK}" && echo -ne "${cor}${2}${SEMCOR}" ;;
-  -ama) cor="${YELLOW}${BLACK}" && echo -e "${cor}${2}${SEMCOR}" ;;
-  -verm) cor="${YELLOW}${BLACK}[!] ${RED}" && echo -e "${cor}${2}${SEMCOR}" ;;
-  -azu) cor="${MAG}${BLACK}" && echo -e "${cor}${2}${SEMCOR}" ;;
-  -verd) cor="${GREEN}${BLACK}" && echo -e "${cor}${2}${SEMCOR}" ;;
-  -bra) cor="${RED}" && echo -ne "${cor}${2}${SEMCOR}" ;;
-  -nazu) cor="${COLOR[6]}${BLACK}" && echo -ne "${cor}${2}${SEMCOR}" ;;
-  -gri) cor="\e[5m\033[1;100m" && echo -ne "${cor}${2}${SEMCOR}" ;;
-  "-bar2" | "-bar") cor="${RED}————————————————————————————————————————————————————" && echo -e "${SEMCOR}${cor}${SEMCOR}" ;;
-  esac
-}
-print_center() {
-  if [[ -z $2 ]]; then
-    text="$1"
-  else
-    col="$1"
-    text="$2"
-  fi
-
-  while read line; do
-    unset space
-    x=$(((54 - ${#line}) / 2))
-    for ((i = 0; i < $x; i++)); do
-      space+=' '
-    done
-    space+="$line"
-    if [[ -z $2 ]]; then
-      msg -azu "$space"
-    else
-      msg "$col" "$space"
-    fi
-  done <<<$(echo -e "$text")
-}
-dependencias() {
-  soft="apache2 cron curl unzip dirmngr apt-transport-https add-apt-repository ppa:ondrej/php update php7.4 php7.2-cli libapache2-mod-php7.4 php7.4-xml php7.4-mcrypt php7.4-curl php7.4-mbstring mariadb-server php7.4-mysql"
-   for i in $soft; do
-    leng="${#i}"
-    puntos=$((21 - $leng))
-    pts="."
-    for ((a = 0; a < $puntos; a++)); do
-      pts+="."
-    done
-    msg -nazu "    Instalando $i$(msg -ama "$pts")"
-    if apt install $i -y &>/dev/null; then
-      msg -verd " INSTALADO"
-    else
-      msg -verm2 " ERRO"
-      sleep 2
-      tput cuu1 && tput dl1
-      print_center -ama "aplicando fix a $i"
-      dpkg --configure -a &>/dev/null
-      sleep 2
-      tput cuu1 && tput dl1
-
-      msg -nazu "    Instalando $i$(msg -ama "$pts")"
-      if apt install $i -y &>/dev/null; then
-        msg -verd " INSTALADO"
-      else
-        msg -verm2 " ERRO"
-      fi
-    fi
-  done
-}
-os_system() {
-  system=$(cat -n /etc/issue | grep 1 | cut -d ' ' -f6,7,8 | sed 's/1//' | sed 's/      //')
-  distro=$(echo "$system" | awk '{print $1}')
-
-  case $distro in
-  Debian) vercion=$(echo $system | awk '{print $3}' | cut -d '.' -f1) ;;
-  Ubuntu) vercion=$(echo $system | awk '{print $2}' | cut -d '.' -f1,2) ;;
-  esac
-}
-install_() {
-  os_system
-  msg -bar
-  echo -e "      \e[5m\033[1;100m   INSTALANDO PACOTES   \033[1;37m"
-  msg -bar
-  print_center -ama "$distro $vercion"
-  print_center -verd "INSTALANDO DEPENDÊNCIAS"
-  msg -bar3
-  dependencias
-  msg -bar3
-  print_center -azu "Removendo pacotes obsoletos"
-  apt autoremove -y &>/dev/null
-  sleep 2
-  tput cuu1 && tput dl1
-  msg -bar
-  print_center -ama "Se algumas das dependências falharem!!!\nQuando terminar, você pode tentar instalar\no mesmo manualmente usando o seguinte comando\napt install nome_do_pacote"
-  msg -bar
-  read -t 60 -n 1 -rsp $'\033[1;39m       << Pressione enter para continuar >>\n'
-}
-inst_base() {
+function inst_base {
+apt install apache2 -y > /dev/null 2>&1
+apt install cron curl unzip dirmngr apt-transport-https -y > /dev/null 2>&1
+add-apt-repository ppa:ondrej/php -y > /dev/null 2>&1
+apt update > /dev/null 2>&1
+apt install php7.4 php7.2-cli libapache2-mod-php7.4 php7.4-xml php7.4-mcrypt php7.4-curl php7.4-mbstring -y > /dev/null 2>&1
 systemctl restart apache2
+apt-get install mariadb-server -y > /dev/null 2>&1
 cd || exit
 mysqladmin -u root password "$pwdroot"
 mysql -u root -p"$pwdroot" -e "UPDATE mysql.user SET Password=PASSWORD('$pwdroot') WHERE User='root'"
@@ -108,6 +19,7 @@ mysql -u root -p"$pwdroot" -e "GRANT ALL PRIVILEGES ON sshplus.* To 'sshplus'@'l
 mysql -u root -p"$pwdroot" -e "FLUSH PRIVILEGES"
 echo '[mysqld]
 max_connections = 1000' >> /etc/mysql/my.cnf
+apt install php7.4-mysql -y > /dev/null 2>&1
 phpenmod mcrypt
 systemctl restart apache2
 ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
@@ -128,7 +40,7 @@ rm gestorssh18.zip index.html > /dev/null 2>&1
 systemctl restart mysql
 clear
 }
-phpmadm() {
+function phpmadm {
 cd /usr/share || exit
 wget https://files.phpmyadmin.net/phpMyAdmin/5.1.0/phpMyAdmin-5.1.0-all-languages.zip > /dev/null 2>&1
 unzip phpMyAdmin-5.1.0-all-languages.zip > /dev/null 2>&1
@@ -140,12 +52,12 @@ rm phpMyAdmin-5.1.0-all-languages.zip
 cd /root || exit
 }
 
-pconf() { 
+function pconf { 
 sed "s/1020/$pwdroot/" /var/www/html/pages/system/pass.php > /tmp/pass
 mv /tmp/pass /var/www/html/pages/system/pass.php
 
 }
-inst_db() { 
+function inst_db { 
 cd || exit
 wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/bdgestorssh.sql > /dev/null 2>&1
 sleep 1
@@ -165,7 +77,7 @@ pweb
 fi
 clear
 }
-cron_set() {
+function cron_set {
 crontab -l > cronset
 echo "
 0 */12 * * * cd /var/www/html/pages/system/ && bash cron.backup.sh && cd /root
@@ -179,7 +91,7 @@ echo "
 @monthly /usr/bin/php /var/www/html/pages/system/cron.limpeza.php" > cronset
 crontab cronset && rm cronset
 }
-fun_swap() {
+function fun_swap {
 			swapoff -a
             rm -rf /bin/ram.img > /dev/null 2>&1
             fallocate -l 4G /bin/ram.img > /dev/null 2>&1
@@ -190,7 +102,7 @@ fun_swap() {
             echo '/bin/ram.img none swap sw 0 0' | sudo tee -a /etc/fstab > /dev/null 2>&1
             sleep 2
 }
-tst_bkp() {
+function tst_bkp {
 cd || exit
 _key=$(echo $(openssl rand -hex 5))
 sed -i "s;49875103u;$_key;g" /var/www/html/pages/system/config.php > /dev/null 2>&1
@@ -211,8 +123,8 @@ echo -e "\n\033[1;36mINICIANDO INSTALAÇÃO \033[1;33mAGUARDE..."
 sleep 6
 clear
 echo "INSTALANDO DEPENDÊNCIAS"
+echo "..."
 sleep 2
-dependencias
 inst_base
 phpmadm
 pconf
